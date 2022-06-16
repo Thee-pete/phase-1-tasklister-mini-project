60b381f295f8840528657e922feac30665ce860e
Peter Ndumia <ndegwandumia@gmail.com>
2022-06-17 00:52:49 +0300

    Done with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  document.querySelector('form').addEventListener('submit', submitInfo)
+
+
+  function submitInfo(e){
+    e.preventDefault()
+   // const inputValue = document.getElementById('new-task-description').value;
+   
+    const inputValue =e.target.new-task-description.value;
+    const listIs = document.getElementById("tasks")
+    const listItem = document.createElement('li')
+    listIs.appendChild(listItem)
+    listItem.appendChild(document.createTextNode(inputValue))
+
+  }
 });
